57c911594f2c885308104664ba0bab8bb32f920e
Michael Critz <git@michaelcritz.com>
2011-02-15 14:28:17 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roborackets::Application.config.secret_token = '5276409564bfca4b12ba8917e417414dd67d9bb5bef04c0ad856f58f2df3c0d7d9defd871816999866c020727b98efe5512cd979f65a315fa7444231fc69abcf'
